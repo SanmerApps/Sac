7a983e010c8e52b3918f1702fec3d670e3f42d1c
SanmerDev <sanmer@sanmer.dev>
2024-02-10 02:24:56 +0800

    Update const

--- app/src/main/kotlin/dev/sanmer/sac/app/Const.kt
+++ app/src/main/kotlin/dev/sanmer/sac/app/Const.kt
@@ -3,6 +3,6 @@ package dev.sanmer.sac.app
 object Const {
     // URL
     const val MY_GITHUB_URL = "https://github.com/SanmerDev"
-    const val TRANSLATE_URL = "https://weblate.sanmer.dev/engage/sac"
+    const val TRANSLATE_URL = "https://weblate.sanmer.app/engage/sac"
     const val GITHUB_URL = "https://github.com/SanmerApps/Sac"
 }
