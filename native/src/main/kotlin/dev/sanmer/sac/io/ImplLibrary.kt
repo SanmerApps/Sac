759be7fb224af6acebb40a240a48448c888aebd7
SanmerDev <sanmer@sanmer.dev>
2024-03-04 19:54:46 +0800

    Rename libname to `sac_jni`

--- native/src/main/kotlin/dev/sanmer/sac/io/ImplLibrary.kt
+++ native/src/main/kotlin/dev/sanmer/sac/io/ImplLibrary.kt
@@ -2,7 +2,7 @@ package dev.sanmer.sac.io
 
 
 internal object ImplLibrary: Library {
-    override val name: String = "sac-jni"
+    override val name: String = "sac_jni"
 
     override fun load() {
         System.loadLibrary(name)
